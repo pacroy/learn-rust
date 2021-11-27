243714c539f65788a19bf3d9040d4774a04ae0b3
Par <pacroy@gmail.com>
2021-11-27 11:12:12 +0700

    Add 10-structures

---
+++ chapter03/10-structures/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("==== structures ====");
+}
+
+// A struct is a collection of fields.
+// A field is simply a data value associated with a data structure. Its value can be of a primitive type or a data structure.
+
+struct SeaCreature {
+    // String is a struct
+    animal_type: String,
+    name: String,
+    arms: i32,
+    legs: i32,
+    weapon: String,
+}
