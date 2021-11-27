05a73bce3e7697064ee9a69272e2816aaba66b22
Par <pacroy@gmail.com>
2021-11-27 11:17:33 +0700

    Add 11-methods

---
+++ chapter03/11-methods/src/main.rs
@@ -0,0 +1,10 @@
+// Unlike functions, methods are functions associated with a specific data type.
+//   static methods — methods that belong to a type itself are called using the :: operator.
+//   instance methods — methods that belong to an instance of a type are called using the . operator.
+
+fn main() {
+    // Using a static method to create an instance of String
+    let s = String::from("Hello world!");
+    // Using a method on the instance
+    println!("{} is {} characters long.", s, s.len());
+}
